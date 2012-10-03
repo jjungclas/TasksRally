1621327d898e85335096ba18a76eab686547f93a
libra guest <jjungclas@nc.rr.com>
2012-10-03 16:49:02 -0400

    Added iteration filtering

--- App.js
+++ App.js
@@ -3,25 +3,48 @@ Ext.define('CustomApp', {
     componentCls: 'app',
 
     _activeFilters: [
-        {
-        property: 'State',
-        operator: '<',
-        value: 'Completed'
-    }
+//        {
+//        property: 'State',
+//        operator: '<',
+//        value: 'Completed'
+//    }
     ],
     _filterGrid: function() {
         this.grid.store.clearFilter();
         this.grid.store.filter(this._activeFilters);
     },
-    _releaseSelected: function(comboBox, records) {
-        var release = records[0].get("_ref");
+    _addFilter: function(filter) {
+        var existingIndex = -1;
+        Ext.Array.forEach(this._activeFilters, function(existingFilter, index) {
+            if (existingFilter.property === filter.property) {
+                existingIndex = index;
+            }
+        });
 
+        if (existingIndex !== -1) {
+            //remove existing filter
+            this._activeFilters.splice(existingIndex, 1);
+        }
+
+        //always add new filter
+        this._activeFilters.push(filter);
+    },
+    _releaseSelected: function(comboBox, records) {
         var releaseFilter = {
             property: 'Release',
-            value: release
+            value: records[0].get("_ref")
         };
 
-        this._activeFilters.push(releaseFilter);
+        this._addFilter(releaseFilter);
+        this._filterGrid();
+    },
+    _iterationSelected: function(comboBox,records){
+        var iterationFilter = {
+            property: 'Iteration',
+            value: records[0].get("_ref")
+        };
+
+        this._addFilter(iterationFilter);
         this._filterGrid();
     },
     launch: function launch() {
@@ -44,7 +67,10 @@ Ext.define('CustomApp', {
                         }
                     }, {
                         xtype: 'rallyiterationcombobox',
-                        fieldLabel: "Iteration"
+                        fieldLabel: "Iteration",
+                        listeners: {
+                            select: Ext.bind(this._iterationSelected, this)
+                        }
                     }, {
                         xtype: 'rallyusercombobox',
                         fieldLabel: 'Owner',
@@ -64,9 +90,9 @@ Ext.define('CustomApp', {
                             projectScopeUp: false,
                             projectScopeDown: true
                         },
-                        filters:this._activeFilters
+                        filters: this._activeFilters
                     }
-                    
+
                 });
 
             }
